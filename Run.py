5383320529c39d29f40d3196cc9c14618755b9fa
B A B A <136027010+BABASEc1@users.noreply.github.com>
2023-10-10 21:50:04 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,12 +2,12 @@ import os,platform
 os.system('git pull')
 os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
 os.system("clear")
-print('\033[1;32m [•] JOIN FB GROUP')
-os.system('xdg-open https://facebook.com/groups/240526195427860/')
+print('\033[1;32m [•] JOIN Facebook Page')
+os.system('xdg-open https://facebook.com/hackerbaba4')
 os.system("clear")
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from Crown import Crown
-    Crown()
+    from Crown import Baba
+    Baba()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
