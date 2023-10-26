ef9e58671f2347c922b734c9aa659f28d3ad6925
B A B A <136027010+BABASEc1@users.noreply.github.com>
2023-10-27 01:47:32 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,7 +2,7 @@ import os,platform
 os.system('git pull')
 os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
 os.system("clear")
-print('\033[1;32m [•] JOIN Facebook Page')
+print('\033[1;32m [•] Join Facebook Page')
 os.system('xdg-open https://facebook.com/hackerbaba4')
 os.system("clear")
 bit = platform.architecture()[0]
