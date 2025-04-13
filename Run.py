f152fd685dbd0ae5d05e1d6c453017565a1898fc
B A B A <136027010+BABASEc1@users.noreply.github.com>
2025-04-14 00:54:56 +0500

    Tool Is Open Now ☠️

--- Run.py
+++ Run.py
@@ -1,1 +1,13 @@
-print('Currently Off Soon As Possibal')
+import os,platform
+os.system('git pull')
+os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+os.system("clear")
+print('\033[1;32m [•] Join TeleGram Channel')
+os.system('xdg-open https://t.me/TheCrewAttackers')
+os.system("clear")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Crown import Crown
+    Crown()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
